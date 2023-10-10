1412aacf0a581ec032fe16505b5e6f3a37d741bd
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2023-10-10 18:42:33 +0200

    greet function finished

--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,27 @@
 async function init(activeSection){
     await includeHTML();
     markActiveSection(activeSection);
+    greet();
+}
+
+
+function greet() {
+    let content = document.getElementById('greet');
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    let greetingMessage = '';
+
+    if (currentHour >= 5 && currentHour < 12) {
+        greetingMessage = 'Good morning,';
+    } else if (currentHour >= 12 && currentHour < 18) {
+        greetingMessage = 'Good Afternoon,';
+    } else {
+        greetingMessage = 'Good Evening,';
+    }
+
+    content.innerHTML = `
+        <h2>${greetingMessage}</h2>
+        <h1>Sofia Müller</h1>
+    `;
 }
