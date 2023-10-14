1b5933a6d9462a3eaa9941d6902b9ebced44d31b
Sebastian Wolff <sebastian.wolff@iu-study.org>
2023-10-14 13:44:10 +0200

    deleted includeHTML function at contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -9,22 +9,6 @@ async function init(activeSection) {
     loadContacts();
 }
 
-
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html");
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
-}
-
-
 function openContactForm() {
     document.querySelector(".content").classList.add("d-none");
     document.querySelector(".add-contact").classList.remove("d-none");
