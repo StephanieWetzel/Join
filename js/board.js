64ec6f891c991caac4c3c5f92473eb61d3748c52
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2023-10-30 17:18:23 +0100

    öffnen einer karte im board angefangen

--- js/board.js
+++ js/board.js
@@ -1,3 +1,14 @@
+const cardCounts = {
+    todo: 0,
+    done: 0,
+    urgent: 0,
+    tasksInBoard: 0,
+    tasksInProgress: 0,
+    awaitingFeedback: 0,
+};
+
+
+
 async function init(activeSection) {
     loadLocalStorageLoggedInUser('loggedInUser');
     await includeHTML();
@@ -71,3 +82,40 @@ function closeTask() {
 
     modal.style.display = "none";
 }
+
+
+// function updateSummaryCounts() {
+//     // Hier sollten Sie den code einfügen, um die cardCounts-Datenstruktur zu aktualisieren
+//     // Zählen Sie die Karten in verschiedenen Abschnitten des Boards und aktualisieren Sie cardCounts
+//     cardCounts.todo = countTodoCards(); // Beispiel: Funktion, um die Anzahl der To-do-Karten zu zählen
+//     cardCounts.done = countDoneCards(); // Beispiel: Funktion, um die Anzahl der Done-Karten zu zählen
+//     cardCounts.urgent = countUrgentCards(); // Beispiel: Funktion, um die Anzahl der Urgent-Karten zu zählen
+//     // usw.
+
+//     // Rufen Sie die greet-Funktion aus der summary.js-Datei auf, um die Zusammenfassung zu aktualisieren
+//     greet();
+// }
+
+// // Diese Funktion zählt die Anzahl der To-do-Karten im Board (Beispiel)
+// function countTodoCards() {
+//     // Hier sollte der Code stehen, um die Anzahl der To-do-Karten im Board zu zählen
+//     // Zum Beispiel, durch Iteration über die Karten und Überprüfung ihres Status
+//     // Geben Sie die Anzahl der To-do-Karten zurück
+//     return 1; // Beispiel: 1 To-do-Karte
+// }
+
+// // Diese Funktion zählt die Anzahl der Done-Karten im Board (Beispiel)
+// function countDoneCards() {
+//     // Hier sollte der Code stehen, um die Anzahl der Done-Karten im Board zu zählen
+//     // Zum Beispiel, durch Iteration über die Karten und Überprüfung ihres Status
+//     // Geben Sie die Anzahl der Done-Karten zurück
+//     return 1; // Beispiel: 1 Done-Karte
+// }
+
+// // Diese Funktion zählt die Anzahl der Urgent-Karten im Board (Beispiel)
+// function countUrgentCards() {
+//     // Hier sollte der Code stehen, um die Anzahl der Urgent-Karten im Board zu zählen
+//     // Zum Beispiel, durch Iteration über die Karten und Überprüfung ihres Status
+//     // Geben Sie die Anzahl der Urgent-Karten zurück
+//     return 1; // Beispiel: 1 Urgent-Karte
+// }
